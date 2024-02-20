feb67023cb693abe6c4ee3218e6512df18347004
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-02-20 14:51:31 +0300

    router and safearea view

--- app/index.js
+++ app/index.js
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, SIZES, images, icons } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  Welcome,
+  ScreenHeaderbtn,
+} from "../components";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>Home</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
